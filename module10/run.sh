896d644e8bf39e709eaa6c5fdf3ac0fff7785f05
Malc5290 <jenbu5290@gmail.com>
2020-04-12 20:43:47 -0400

    Module10 Assignment

---
+++ module10/run.sh
@@ -0,0 +1,15 @@
+make
+printf "\nPerforming 5 runs...\n"
+printf "Run 1:\n"
+./assignment.exe --no-print 
+printf "Run 2:\n"
+./assignment.exe --no-print
+printf "Run 3:\n"
+./assignment.exe --no-print 
+printf "Run 4:\n"
+./assignment.exe --no-print 
+printf "Run 5:\n"
+./assignment.exe --no-print 
+
+#printf "\nLarge Matrix 4096 x 4096\n"
+#./assignment.exe 4096
